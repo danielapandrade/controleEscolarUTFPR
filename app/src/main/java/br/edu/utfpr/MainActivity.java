b8ec0044f96c9de8381b95a2abbfafec608d5f88
daniela <danielandraderock@gmail.com>
2023-02-08 18:56:22 -0300

    finalizando duas primeiras interfaces

--- app/src/main/java/br/edu/utfpr/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton cadastroDependente;
+
+    private ImageButton cadastroEvento;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +19,18 @@ public class MainActivity extends AppCompatActivity {
         cadastroDependente = findViewById(R.id.imageButtonCadastrarDependente);
         cadastroDependente.setOnClickListener(v->abrirCadastroDependente());
 
+        cadastroEvento = findViewById(R.id.imageButtonCadastrarEvento);
+        cadastroEvento.setOnClickListener(v->abrirCadastroEvento());
+
     }
 
     public void abrirCadastroDependente(){
         Intent intent = new Intent(this, CadastroDependenteActivity.class);
+        startActivity(intent);
+    }
+
+    public void abrirCadastroEvento(){
+        Intent intent = new Intent(this, CadastroEventoActivity.class);
         startActivity(intent);
     }
 }
