ee63e37166b574312fba93f787000c82e7d18ffb
daniela <danielandraderock@gmail.com>
2023-02-10 22:20:11 -0300

    finalizando requisitos da atividade 2

--- app/src/main/java/br/edu/utfpr/CadastroDependenteActivity.java
+++ app/src/main/java/br/edu/utfpr/CadastroDependenteActivity.java
@@ -3,19 +3,47 @@ package br.edu.utfpr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 public class CadastroDependenteActivity extends AppCompatActivity {
 
+    private EditText nome;
+    private EditText escola;
+    private EditText idade;
+
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_dependente);
 
-
-
-        
+        nome = findViewById(R.id.editTextNomeDependente);
+        escola = findViewById(R.id.editTextEscola);
+        idade = findViewById(R.id.editTextNumberIdade);
+        radioGroup = findViewById(R.id.radioGroupSerie);
 
     }
+
+    public void limpar (View view){
+        nome.setText("");
+        escola.setText("");
+        idade.setText("");
+        radioGroup.clearCheck();
+        nome.requestFocus();
+    }
+
+    public void salvar (View view){
+        if(nome.getText().equals("")||radioGroup.getCheckedRadioButtonId()!=0||
+                escola.getText().equals("")||idade.getText().equals("")){
+
+            Toast.makeText(this, "Não pode haver campos vazios!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
--- app/src/main/java/br/edu/utfpr/CadastroEventoActivity.java
+++ app/src/main/java/br/edu/utfpr/CadastroEventoActivity.java
@@ -3,12 +3,75 @@ package br.edu.utfpr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class CadastroEventoActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+
+    private EditText evento;
+
+    private EditText escola;
+
+    private EditText data;
+
+    private CheckBox checkBoxComida;
+
+    private CheckBox checkBoxBebida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_evento);
+
+        evento = findViewById(R.id.editTextEvento);
+        escola = findViewById(R.id.editTextEscolaEvento);
+        data = findViewById(R.id.editTextDataEvento);
+        checkBoxComida = findViewById(R.id.checkBoxComida);
+        checkBoxBebida = findViewById(R.id.checkBoxBebida);
+
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.tipo_evento,
+                android.R.layout.simple_spinner_item);
+
+        spinner = (Spinner) findViewById(R.id.spinnerEvento);
+        spinner.setAdapter(adapter);
     }
+
+    public void limpar(View view) {
+        escola.setText("");
+        evento.setText("");
+        data.setText("");
+        checkBoxComida.setChecked(false);
+        checkBoxBebida.setChecked(false);
+        evento.requestFocus();
+
+        Toast.makeText(this, "Valores deletados!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void salvar(View view) {
+        if (escola.getText().equals("") || evento.getText().equals("") ||
+                data.getText().equals("") || verificarcheckBoxLevar()) {
+
+            Toast.makeText(this, "Não pode haver campos vazios!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean verificarcheckBoxLevar() {
+        if (!checkBoxBebida.isChecked() && !checkBoxComida.isChecked()) {
+            return true;
+
+        }
+
+        return false;
+    }
+
+
 }
