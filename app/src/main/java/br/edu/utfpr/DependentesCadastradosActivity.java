3727da91cd7f2a13fbb6b8fc8df85e1f6a32aede
daniela <danielandraderock@gmail.com>
2023-02-14 12:54:08 -0300

    adicionando list view

--- app/src/main/java/br/edu/utfpr/DependentesCadastradosActivity.java
+++ app/src/main/java/br/edu/utfpr/DependentesCadastradosActivity.java
@@ -3,12 +3,15 @@ package br.edu.utfpr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class DependentesCadastradosActivity extends AppCompatActivity {
 
+    private ListView listaDependentes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dependentes_cadastrados);
+        listaDependentes = findViewById(R.id.listViewDependentes);
     }
 }
